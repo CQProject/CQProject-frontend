f04aea1f0516a88373401bbb1687c7306ba01b7e
diogosantosmendes <diogo.santos.mendes@hotmail.com>
2017-08-23 19:55:21 +0100

    schools layout

--- src/app/schools/school-home.component.ts
+++ src/app/schools/school-home.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SchoolService } from './school.service';
 import { School } from "./iSchool";
 
 @Component({ templateUrl: "./school-home.component.html" })
 
-export class SchoolHomeComponent{
+export class SchoolHomeComponent {
 
     public schools: School[];
 
@@ -14,11 +14,25 @@ export class SchoolHomeComponent{
         private _route: Router
     ) { }
 
-    ngOnInit() {
+    public ngOnInit() {
         this._service.getSchools()
             .subscribe(
-                schools => {this.schools = schools; console.log(schools);},
-                error => console.log("Impossível carregar lista de escolas"));
-        
+            schools => {
+                this.schools = schools;
+                for (let school of this.schools) {
+                    school.Logo = "../../assets/img/" + school.Logo;
+                    school.ProfilePicture = "../../assets/img/" + school.ProfilePicture;
+                }
+                console.log(schools);
+            },
+            error => console.log("Impossível carregar lista de escolas"));
+    }
+
+    public show(elementID: string) {
+        document.getElementById(elementID).style.display='block';
+    }
+
+    public hide(elementID: string) {
+        document.getElementById(elementID).style.display='none';
     }
 }
