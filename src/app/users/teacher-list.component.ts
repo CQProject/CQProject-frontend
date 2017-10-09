0832aa3a7b141554d3e42862d53a382b18741b01
ricardma <aluno18774@ipt.pt>
2017-10-09 14:18:42 +0100

    ...

---
+++ src/app/users/teacher-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Renderer } from '@angular/core';
+import { ActivatedRoute, Resolve, Router } from '@angular/router';
+
+import { UserService } from '../users/user.service';
+import { FileService } from '../utils/files.service';
+import { ClassService } from "../classes/class.service";
+import { UserProfile } from './iUsers';
+import { ParentingService } from './parenting.service';
+declare var $: any;
+
+@Component({
+    selector: "teacher-list",
+    templateUrl: "./teacher-list.component.html"
+})
+
+export class TeacherListComponent {
+
+    public teachersID: any[];
+    public teachers: UserProfile[];
+
+    constructor(
+        private _classService: ClassService,
+        private _userService: UserService,
+        private _fileService: FileService,
+        private _router: Router,
+        private _route: ActivatedRoute,
+    ) { this.teachersID = [];this.teachers=[]; }
+
+    public async ngOnInit(){
+        let userID = JSON.parse(localStorage.getItem('currentUser')).userID;
+        let nPages = await this._userService.getUsersCount(2);
+        for(var i = 0; i < nPages; i++){
+            this.teachersID = this.teachersID.concat(await this._userService.getUsersByPage(i,2));
+        }
+        for(var i = 0; i < this.teachersID.length; i++){
+            this.teachers.push(await this._userService.getProfile(this.teachersID[i]))
+        }
+    }
+}
